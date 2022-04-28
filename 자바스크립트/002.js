25f9b15b076f866fbb73a6b7fe8d3ae9894ede70
Nam-Wijeong <dnlwjdv@hanmail.net>
2022-04-28 18:47:33 +0900

    0428 update

---
+++ 자바스크립트/002.js
@@ -0,0 +1,5 @@
+document.getElementById('one').innerHTML =
+        'hello <strong>world document</strong>';
+      console.log('hello world console');
+      window.alert('hello world alert창!');
+      document.write('hello~!~!');
